a56595c6fcf654648b3c1ed109c4bb3b96d5368b
Xie Zhihao <xzh1206@gmail.com>
2019-08-31 01:04:10 +0800

    Add mode

---
+++ src/class/Mode.js
@@ -0,0 +1,24 @@
+class Mode {
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getValue() {
+    return this.value;
+  }
+}
+
+Mode.regular = new Mode("regular", 1);
+Mode.ranked = new Mode("ranked", 2);
+Mode.league = new Mode("league", 3);
+Mode.private = new Mode("private", 4);
+Mode.splatfest = new Mode("splatfest", 5);
+
+Object.freeze(Mode);
+
+export {Mode};
