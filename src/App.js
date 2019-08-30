c9d595318956bd10f96bd5d32cefd5d9e84ed738
Xie Zhihao <xzh1206@gmail.com>
2019-08-30 14:06:49 +0800

    Optimize layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Layout, Menu, Icon } from 'antd';
 const { Sider, Content } = Layout;
 const { SubMenu } = Menu;
 
-class Sidebar extends React.Component {
+class App extends React.Component {
   state = {
     collapsed: false,
   };
@@ -22,70 +22,75 @@ class Sidebar extends React.Component {
 
   render() {
     return (
-      <Layout style={{ minHeight: '100vh'}}>
-        <Sider
-          breakpoint='md'
-          collapsible collapsed={this.state.collapsed}
-          onBreakpoint={this.onBreakpoint}
-          onCollapse={this.onCollapse}
-          style={{
-            minHeight: '100vh'
-          }}
-        >
-          <div className="App-logo">
-            <img src={logo} alt="logo" />
-          </div>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="home" />
-              <span>Home</span>
-            </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="schedule" />
-                  <span>Schedules</span>
-                </span>
-              }
-            >
-              <Menu.Item key="2">Regular Battle</Menu.Item>
-              <Menu.Item key="3">Ranked Battle</Menu.Item>
-              <Menu.Item key="4">League Battle</Menu.Item>
-              <Menu.Item key="5">Salmon Run</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="line-chart" />
-                  <span>Statistics</span>
-                </span>
-              }
-            >
-              <Menu.Item key="6">Stages</Menu.Item>
-              <Menu.Item key="7">Weapons</Menu.Item>
-              <Menu.Item key="8">Battle</Menu.Item>
-              <Menu.Item key="9">Salmon Run</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="10">
-              <Icon type="menu" />
-              <span>Battle</span>
-            </Menu.Item>
-            <Menu.Item key="11">
-              <Icon type="menu" />
-              <span>Salmon Run</span>
-            </Menu.Item>
-            <Menu.Item key="12">
-              <Icon type="setting" />
-              <span>Settings</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Content className="App-content" style={{ height: '100vh' }} />
+      <Layout>
+        <div className="App-sider-container">
+          <Sider
+            breakpoint='md'
+            collapsible collapsed={this.state.collapsed}
+            onBreakpoint={this.onBreakpoint}
+            onCollapse={this.onCollapse}
+            style={{
+              height: '100vh',
+              position: 'relative'
+            }}
+          >
+            <div className="App-logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">
+                <Icon type="home" />
+                <span>Home</span>
+              </Menu.Item>
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <Icon type="schedule" />
+                    <span>Schedules</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="2">Regular Battle</Menu.Item>
+                <Menu.Item key="3">Ranked Battle</Menu.Item>
+                <Menu.Item key="4">League Battle</Menu.Item>
+                <Menu.Item key="5">Salmon Run</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="line-chart" />
+                    <span>Statistics</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="6">Stages</Menu.Item>
+                <Menu.Item key="7">Weapons</Menu.Item>
+                <Menu.Item key="8">Battle</Menu.Item>
+                <Menu.Item key="9">Salmon Run</Menu.Item>
+              </SubMenu>
+              <Menu.Item key="10">
+                <Icon type="menu" />
+                <span>Battle</span>
+              </Menu.Item>
+              <Menu.Item key="11">
+                <Icon type="menu" />
+                <span>Salmon Run</span>
+              </Menu.Item>
+              <Menu.Item key="12">
+                <Icon type="setting" />
+                <span>Settings</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+        </div>
+        <Content className="App-content" style={{ height: '100vh' }}>
+          <Content style={{ background: '#fff', height: '2000px', margin: '16px' }} />
+        </Content>
       </Layout>
     );
   }
 }
 
-export default Sidebar;
+export default App;
