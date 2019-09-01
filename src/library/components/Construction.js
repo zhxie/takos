1bd5522d97d66612b5bd7a2da7ad27a7bd6a783a
Xie Zhihao <xzh1206@gmail.com>
2019-09-02 00:45:19 +0800

    Reconstruct construction

--- src/library/components/Construction.js
+++ src/library/components/Construction.js
@@ -1,40 +1,28 @@
 import React from 'react';
-import { Layout, Result } from 'antd';
+import { Result } from 'antd';
 
 import './Construction.css';
 import OctolingsKillIcon from './CustomIcons';
 
-const { Content } = Layout;
-
 class Construction extends React.Component {
   render() {
     return (
-      <Layout>
-        <Content
-          id="Construction-content"
-          className="Construction-content"
-          style={{
-            background: '#fff',
-            height: 'calc(100vh - 32px)',
-            margin: '16px'
-          }}
-        >
-          <Result
-            icon={
-              <OctolingsKillIcon
-                className="Construction-content-result-icon"
-                style={{
-                  width: '2em',
-                  fill: '#e6f7ff',
-                  stroke: '#1890ff'
-                }}
-              />
-            }
-            title="In Painting"
-            subTitle="This page is under construction."
-          />
-        </Content>
-      </Layout>
+      <div className="Construction-content">
+        <Result
+          icon={
+            <OctolingsKillIcon
+              className="Construction-content-result-icon"
+              style={{
+                width: '2em',
+                fill: '#e6f7ff',
+                stroke: '#1890ff'
+              }}
+            />
+          }
+          title="In Painting"
+          subTitle="This page is under construction."
+        />
+      </div>
     );
   }
 }
