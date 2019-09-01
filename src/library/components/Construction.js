76b4a160df52210a96cf300fff88f70de9a49615
Xie Zhihao <xzh1206@gmail.com>
2019-09-01 21:31:38 +0800

    Add construction page

--- src/library/components/Construction.js
+++ src/library/components/Construction.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Result } from 'antd';
+
+import './Construction.css';
+import OctolingsKillIcon from './CustomIcons';
 
 const { Content } = Layout;
 
 class Construction extends React.Component {
   render() {
     return (
-      <Content
-        id="Construction-content"
-        className="Construction-content"
-        style={{
-          background: '#fff',
-          height: 'calc(100vh - 32px)',
-          margin: '16px'
-        }}
-      />
+      <Layout>
+        <Content
+          id="Construction-content"
+          className="Construction-content"
+          style={{
+            background: '#fff',
+            height: 'calc(100vh - 32px)',
+            margin: '16px'
+          }}
+        >
+          <Result
+            icon={
+              <OctolingsKillIcon
+                className="Construction-content-result-icon"
+                style={{
+                  width: '2em',
+                  fill: '#e6f7ff',
+                  stroke: '#1890ff'
+                }}
+              />
+            }
+            title="In Painting"
+            subTitle="This page is under construction."
+          />
+        </Content>
+      </Layout>
     );
   }
 }
