8d3338d4e78bbf818cfecda0f96f56a0aabb34a6
Xie Zhihao <xzh1206@gmail.com>
2019-09-07 13:03:42 +0800

    Fix string.format not working

--- src/library/components/TimeConverter.js
+++ src/library/components/TimeConverter.js
@@ -1,10 +1,12 @@
+import './StringHelper';
+
 class TimeConverter {
   static getScheduleTime = time => {
     var date = new Date(time * 1000);
     var hours = date.getHours();
     var minutes = '0' + date.getMinutes();
     var seconds = '0' + date.getSeconds();
-    return '{0}:{1}:{2}'.formet(hours, minutes.substr(-2), seconds.substr(-2));
+    return '{0}:{1}:{2}'.format(hours, minutes.substr(-2), seconds.substr(-2));
   };
 
   static getSchedulePeriod = (startTime, endTime) => {
