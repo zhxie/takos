f8fab9c08eb0b8923035541429911f028b6b6fa9
Xie Zhihao <xzh1206@gmail.com>
2019-09-22 16:50:03 +0800

    Match url and sider

--- src/MainWindow.js
+++ src/MainWindow.js
@@ -15,11 +15,44 @@ const { SubMenu } = Menu;
 
 class MainWindow extends React.Component {
   state = {
-    collapsed: false
+    collapsed: false,
+    selected: []
   };
 
   onCollapse = collapsed => {
     this.setState({ collapsed });
+  };
+
+  refreshMenu = () => {
+    if (this.props.location.pathname.startsWith('/dashboard')) {
+      this.setState({ selected: ['dashboard'] });
+    } else if (this.props.location.pathname.startsWith('/schedules/regular')) {
+      this.setState({ selected: ['schedules.regular_battle'] });
+    } else if (this.props.location.pathname.startsWith('/schedules/ranked')) {
+      this.setState({ selected: ['schedules.ranked_battle'] });
+    } else if (this.props.location.pathname.startsWith('/schedules/league')) {
+      this.setState({ selected: ['schedules.league_battle'] });
+    } else if (this.props.location.pathname.startsWith('/schedules/salmon')) {
+      this.setState({ selected: ['schedules.salmon_run'] });
+    } else if (this.props.location.pathname.startsWith('/stats/stages')) {
+      this.setState({ selected: ['statistics.stages'] });
+    } else if (this.props.location.pathname.startsWith('/stats/weapons')) {
+      this.setState({ selected: ['statistics.weapons'] });
+    } else if (this.props.location.pathname.startsWith('/stats/battles')) {
+      this.setState({ selected: ['statistics.battles'] });
+    } else if (this.props.location.pathname.startsWith('/stats/salmon')) {
+      this.setState({ selected: ['statistics.salmon_run'] });
+    } else if (this.props.location.pathname.startsWith('/battles')) {
+      this.setState({ selected: ['battles'] });
+    } else if (this.props.location.pathname.startsWith('/salmon')) {
+      this.setState({ selected: ['salmon_run'] });
+    } else if (this.props.location.pathname.startsWith('/shop')) {
+      this.setState({ selected: ['gear_shop'] });
+    } else if (this.props.location.pathname.startsWith('/settings')) {
+      this.setState({ selected: ['settings'] });
+    } else {
+      this.setState({ selected: [] });
+    }
   };
 
   renderLogo = () => {
@@ -47,14 +80,14 @@ class MainWindow extends React.Component {
             <div className="MainWindow-logo">
               <img src={logo} alt="logo" />
             </div>
-            <Menu theme="dark" mode="vertical">
-              <Menu.Item key="1">
+            <Menu selectedKeys={this.state.selected} theme="dark" mode="vertical">
+              <Menu.Item key="dashboard">
                 <Icon type="dashboard" />
                 <FormattedMessage id="app.dashboard" defaultMessage="Dashboard" />
                 <Link to="/dashboard" />
               </Menu.Item>
               <SubMenu
-                key="sub1"
+                key="schedules"
                 title={
                   <span>
                     <Icon type="schedule" />
@@ -62,25 +95,25 @@ class MainWindow extends React.Component {
                   </span>
                 }
               >
-                <Menu.Item key="2">
+                <Menu.Item key="schedules.regular_battle">
                   <FormattedMessage id="mode.regular_battle" defaultMessage="Regular Battle" />
                   <Link to="/schedules/regular" />
                 </Menu.Item>
-                <Menu.Item key="3">
+                <Menu.Item key="schedules.ranked_battle">
                   <FormattedMessage id="mode.ranked_battle" defaultMessage="Ranked Battle" />
                   <Link to="/schedules/ranked" />
                 </Menu.Item>
-                <Menu.Item key="4">
+                <Menu.Item key="schedules.league_battle">
                   <FormattedMessage id="mode.league_battle" defaultMessage="League Battle" />
                   <Link to="/schedules/league" />
                 </Menu.Item>
-                <Menu.Item key="5">
+                <Menu.Item key="schedules.salmon_run">
                   <FormattedMessage id="app.salmon_run" defaultMessage="Salmon Run" />
                   <Link to="/schedules/salmon" />
                 </Menu.Item>
               </SubMenu>
               <SubMenu
-                key="sub2"
+                key="stats"
                 title={
                   <span>
                     <Icon type="line-chart" />
@@ -88,39 +121,39 @@ class MainWindow extends React.Component {
                   </span>
                 }
               >
-                <Menu.Item key="6">
+                <Menu.Item key="statistics.stages">
                   <FormattedMessage id="app.stages" defaultMessage="Stages" />
                   <Link to="/stats/stages" />
                 </Menu.Item>
-                <Menu.Item key="7">
+                <Menu.Item key="statistics.weapons">
                   <FormattedMessage id="app.weapons" defaultMessage="Weapons" />
                   <Link to="/stats/weapons" />
                 </Menu.Item>
-                <Menu.Item key="8">
+                <Menu.Item key="statistics.battles">
                   <FormattedMessage id="app.battles" defaultMessage="Battles" />
                   <Link to="/stats/battles" />
                 </Menu.Item>
-                <Menu.Item key="9">
+                <Menu.Item key="statistics.salmon">
                   <FormattedMessage id="app.salmon_run" defaultMessage="Salmon Run" />
                   <Link to="/stats/salmon" />
                 </Menu.Item>
               </SubMenu>
-              <Menu.Item key="10">
+              <Menu.Item key="battles">
                 <Icon type="menu" />
                 <FormattedMessage id="app.battles" defaultMessage="Battles" />
                 <Link to="/battles" />
               </Menu.Item>
-              <Menu.Item key="11">
+              <Menu.Item key="salmon">
                 <Icon type="menu" />
                 <FormattedMessage id="app.salmon_run" defaultMessage="Salmon Run" />
                 <Link to="/salmon" />
               </Menu.Item>
-              <Menu.Item key="12">
+              <Menu.Item key="gear_shop">
                 <Icon type="shopping" />
                 <FormattedMessage id="app.gear_shop" defaultMessage="Gear Shop" />
                 <Link to="/shop" />
               </Menu.Item>
-              <Menu.Item key="13">
+              <Menu.Item key="settings">
                 <Icon type="setting" />
                 <FormattedMessage id="app.settings" defaultMessage="Settings" />
                 <Link to="/settings" />
@@ -147,6 +180,16 @@ class MainWindow extends React.Component {
         </Content>
       </Layout>
     );
+  }
+
+  componentDidMount() {
+    this.refreshMenu();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      this.refreshMenu();
+    }
   }
 }
 
