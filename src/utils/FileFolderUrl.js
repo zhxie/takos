02525022811ac0ed055b643463b91ce8d673af21
Xie Zhihao <xzh1206@gmail.com>
2019-10-11 10:56:34 +0800

    Add switch to turn on or off proxy

--- src/utils/FileFolderUrl.js
+++ src/utils/FileFolderUrl.js
@@ -1,9 +1,11 @@
 const USER_AGENT = 'Takos/0.1.0';
 
-const SPLATOON2_INK = '/splatoon2inkApi';
+const SPLATOON2_INK = 'https://splatoon2.ink';
+const SPLATOON2_INK_PROXY = '/splatoon2inkApi';
 const SPLATOON2_INK_SCHEDULES = '/data/schedules.json';
 
-const SPLATNET = '/splatnetApi';
+const SPLATNET = 'https://app.splatoon2.nintendo.net';
+const SPLATNET_PROXY = '/splatnetApi';
 const SPLATNET_RESULTS = '/api/results';
 const SPLATNET_RESULT = '/api/results/{0}';
 const SPLATNET_SHARE_RESULT = '/api/share/results/{0}';
@@ -12,49 +14,65 @@ const SPLATNET_NICKNAME_AND_ICON = '/api/nickname_and_icon?id={0}';
 const NINTENDO_ACCOUNTS_AUTHORIZE =
   'https://accounts.nintendo.com/connect/1.0.0/authorize?state={0}&redirect_uri=npf71b963c1b7b6d119%3A%2F%2Fauth&client_id=71b963c1b7b6d119&scope=openid+user+user.birthday+user.mii+user.screenName&response_type=session_token_code&session_token_code_challenge={1}&session_token_code_challenge_method=S256&theme=login_form';
 
-const NINTENDO_ACCOUNTS = '/nintendoAccountsApi';
+const NINTENDO_ACCOUNTS = 'https://accounts.nintendo.com';
+const NINTENDO_ACCOUNTS_PROXY = '/nintendoAccountsApi';
 const NINTENDO_ACCOUNTS_SESSION_TOKEN = '/connect/1.0.0/api/session_token';
 const NINTENDO_ACCOUNTS_TOKEN = '/connect/1.0.0/api/token';
 
-const NINTENDO_ACCOUNTS_API = '/nintendoAccountsApiApi';
+const NINTENDO_ACCOUNTS_API = 'https://api.accounts.nintendo.com';
+const NINTENDO_ACCOUNTS_API_PROXY = '/nintendoAccountsApiApi';
 const NINTENDO_ACCOUNTS_API_USER_INFO = '/2.0.0/users/me';
 
-const ELI_FESSLER = '/eliFesslerApi';
+const ELI_FESSLER = 'https://elifessler.com';
+const ELI_FESSLER_PROXY = '/eliFesslerApi';
 const ELI_FESSLER_GEN2 = '/s2s/api/gen2';
 
-const FLAPG = '/flapgApi';
+const FLAPG = 'https://flapg.com';
+const FLAPG_PROXY = '/flapgApi';
 const FLAPG_LOGIN = '/ika2/api/login';
 
-const NINTENDO_SERVICE = '/nintendoServiceApi';
+const NINTENDO_SERVICE = 'https://api-lp1.znc.srv.nintendo.net';
+const NINTENDO_SERVICE_PROXY = '/nintendoServiceApi';
 const NINTENDO_SERVICE_LOGIN = '/v1/Account/Login';
 const NINTENDO_SERVICE_WEB_SERVICE_TOKEN = '/v2/Game/GetWebServiceToken';
 
 class FileFolderUrl {
+  // Set to true to proxy through http-proxy-middleware
+  static useProxy = true;
+
   static get USER_AGENT() {
     return USER_AGENT;
   }
 
   static get SPLATOON2_INK() {
-    return SPLATOON2_INK;
+    if (FileFolderUrl.useProxy) {
+      return SPLATOON2_INK_PROXY;
+    } else {
+      return SPLATOON2_INK;
+    }
   }
   static get SPLATOON2_INK_SCHEDULES() {
-    return SPLATOON2_INK + SPLATOON2_INK_SCHEDULES;
+    return FileFolderUrl.SPLATOON2_INK + SPLATOON2_INK_SCHEDULES;
   }
 
   static get SPLATNET() {
-    return SPLATNET;
+    if (FileFolderUrl.useProxy) {
+      return SPLATNET_PROXY;
+    } else {
+      return SPLATNET;
+    }
   }
   static get SPLATNET_RESULTS() {
-    return SPLATNET + SPLATNET_RESULTS;
+    return FileFolderUrl.SPLATNET + SPLATNET_RESULTS;
   }
   static get SPLATNET_RESULT() {
-    return SPLATNET + SPLATNET_RESULT;
+    return FileFolderUrl.SPLATNET + SPLATNET_RESULT;
   }
   static get SPLATNET_SHARE_RESULT() {
-    return SPLATNET + SPLATNET_SHARE_RESULT;
+    return FileFolderUrl.SPLATNET + SPLATNET_SHARE_RESULT;
   }
   static get SPLATNET_NICKNAME_AND_ICON() {
-    return SPLATNET + SPLATNET_NICKNAME_AND_ICON;
+    return FileFolderUrl.SPLATNET + SPLATNET_NICKNAME_AND_ICON;
   }
 
   static get NINTENDO_ACCOUNTS_AUTHORIZE() {
@@ -62,44 +80,64 @@ class FileFolderUrl {
   }
 
   static get NINTENDO_ACCOUNTS() {
-    return NINTENDO_ACCOUNTS;
+    if (FileFolderUrl.useProxy) {
+      return NINTENDO_ACCOUNTS_PROXY;
+    } else {
+      return NINTENDO_ACCOUNTS;
+    }
   }
   static get NINTENDO_ACCOUNTS_SESSION_TOKEN() {
-    return NINTENDO_ACCOUNTS + NINTENDO_ACCOUNTS_SESSION_TOKEN;
+    return FileFolderUrl.NINTENDO_ACCOUNTS + NINTENDO_ACCOUNTS_SESSION_TOKEN;
   }
   static get NINTENDO_ACCOUNTS_TOKEN() {
-    return NINTENDO_ACCOUNTS + NINTENDO_ACCOUNTS_TOKEN;
+    return FileFolderUrl.NINTENDO_ACCOUNTS + NINTENDO_ACCOUNTS_TOKEN;
   }
 
   static get NINTENDO_ACCOUNTS_API() {
-    return NINTENDO_ACCOUNTS_API;
+    if (FileFolderUrl.useProxy) {
+      return NINTENDO_ACCOUNTS_API_PROXY;
+    } else {
+      return NINTENDO_ACCOUNTS_API;
+    }
   }
   static get NINTENDO_ACCOUNTS_API_USER_INFO() {
-    return NINTENDO_ACCOUNTS_API + NINTENDO_ACCOUNTS_API_USER_INFO;
+    return FileFolderUrl.NINTENDO_ACCOUNTS_API + NINTENDO_ACCOUNTS_API_USER_INFO;
   }
 
   static get ELI_FESSLER() {
-    return ELI_FESSLER;
+    if (FileFolderUrl.useProxy) {
+      return ELI_FESSLER_PROXY;
+    } else {
+      return ELI_FESSLER;
+    }
   }
   static get ELI_FESSLER_GEN2() {
-    return ELI_FESSLER + ELI_FESSLER_GEN2;
+    return FileFolderUrl.ELI_FESSLER + ELI_FESSLER_GEN2;
   }
 
   static get FLAPG() {
-    return FLAPG;
+    if (FileFolderUrl.useProxy) {
+      return FLAPG_PROXY;
+    } else {
+      return FLAPG;
+    }
   }
   static get FLAPG_LOGIN() {
-    return FLAPG + FLAPG_LOGIN;
+    return FileFolderUrl.FLAPG + FLAPG_LOGIN;
   }
 
   static get NINTENDO_SERVICE() {
-    return NINTENDO_SERVICE;
+    if (FileFolderUrl.useProxy) {
+      return NINTENDO_SERVICE_PROXY;
+    } else {
+      return NINTENDO_SERVICE;
+    }
   }
   static get NINTENDO_SERVICE_LOGIN() {
-    return NINTENDO_SERVICE + NINTENDO_SERVICE_LOGIN;
+    return FileFolderUrl.NINTENDO_SERVICE + NINTENDO_SERVICE_LOGIN;
   }
   static get NINTENDO_SERVICE_WEB_SERVICE_TOKEN() {
-    return NINTENDO_SERVICE + NINTENDO_SERVICE_WEB_SERVICE_TOKEN;
+    return FileFolderUrl.NINTENDO_SERVICE + NINTENDO_SERVICE_WEB_SERVICE_TOKEN;
   }
 }
 
